6b6a0cfc998718a8b3583325809e9b397b7a189e
Hassan Zaib Hayat <hassanzaibhayatske@gmail.com>
2026-01-09 00:34:56 +0500

    chore: add docker and compose with postgres

---
+++ README.md
@@ -0,0 +1,7 @@
+# django-enterprise-backend
+
+## Local Run (Docker)
+
+1. `cp .env.example .env`
+2. `docker compose up --build`
+3. Open http://localhost:8000
--- src/config/settings.py
+++ src/config/settings.py
@@ -88,12 +88,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DB_HOST = os.getenv("DB_HOST")
+if DB_HOST:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": DB_HOST,
+            "PORT": os.getenv("DB_PORT"),
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
